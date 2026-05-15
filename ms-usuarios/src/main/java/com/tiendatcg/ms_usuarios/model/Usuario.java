28f4c7f34576a7c409d557a768f2673dfbf4e48f
Eduardo Cossio <ed.cossio@duocuc.cl>
2026-05-15 09:28:08 -0400

    feat: actualizacion usuarios por errores

---
+++ ms-usuarios/src/main/java/com/tiendatcg/ms_usuarios/model/Usuario.java
@@ -0,0 +1,45 @@
+package com.tiendatcg.ms_usuarios.model;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name="usuarios")
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Usuario {
+
+    // atributo id
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idPerfil;
+
+    /*@Column(nullable = false, unique = true)
+    private Long idAuthRef; // Puntero lógico a MS-Auth*/
+
+    @Column(nullable = false, length = 150)
+    private String nombreCompleto;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String correoElectronico;
+
+    @Column(length = 250)
+    private String direccionFisica; // Opcional
+
+    // Relación 1:1 física con la tabla de puntos
+    @OneToOne(mappedBy = "perfil", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Fidelidad puntos;
+}
+
---
+++ ms-usuarios/src/main/java/com/tiendatcg/ms_usuarios/repository/UsuarioRepository.java
@@ -0,0 +1,15 @@
+package com.tiendatcg.ms_usuarios.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.tiendatcg.ms_usuarios.model.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario,Long>{
+
+    Optional<Usuario> findByEmail(String correoElectronico);
+    Optional<Usuario> findByNombreUsuario(String nombreUsuario);
+}
